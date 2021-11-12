7a95eaf50dcf4aec6f9afec638a88999d5d693f5
Sagar Kaurav <sagar.kaurav@gmail.com>
2021-11-12 09:57:24 +0530

    change credit url

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
       Coded by <a className="text-blue-600" href="https://www.frontendmentor.io/profile/sagarkaurav">Sagar Kaurav</a>.
             <a className="text-blue-600" href="https://github.com/sagarkaurav">Github</a><br />
             Icons by <a className="text-blue-600" href="https://icons.getbootstrap.com/" rel="noopener noreferrer" target="_blank">Bootstrap Icons</a><br />
-            API by <a className="text-blue-600" href="https://restcountries.eu" rel="noopener noreferrer" target="_blank">https://restcountries.eu</a><br />
+            API by <a className="text-blue-600" href="https://restcountries.com" rel="noopener noreferrer" target="_blank">https://restcountries.com/v2/all</a><br />
         </div>
     );
 }
